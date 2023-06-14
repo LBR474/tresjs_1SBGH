77e4695d21e4952c34157d5a847a155efbdf5054
Fake Name <fakename@fake.com>
2023-06-15 09:46:49 +1000

    Extra commit

--- src/components/LoadModelsPage.vue
+++ src/components/LoadModelsPage.vue
@@ -1,30 +1,26 @@
 <script setup lang="ts">
-import { ref, shallowRef, ShallowRef } from 'vue';
-import { Color } from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { ref, shallowRef, ShallowRef } from "vue";
+import { Color } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-import { useLoader, useRenderLoop, TresObject } from '@tresjs/core';
-import TresMesh from '@tresjs/core'
-import { OrbitControls, GLTFModel } from '@tresjs/cientos';
+import { useLoader, useRenderLoop, TresObject } from "@tresjs/core";
+import TresMesh from "@tresjs/core";
+import { OrbitControls, GLTFModel } from "@tresjs/cientos";
 
-import * as THREE from 'three'
+import * as THREE from "three";
 
-const bgColor = new Color('#82DBC5');
+const bgColor = new Color("#82DBC5");
 
 const meshPosition = [0, 3, 0];
 
 const lightRef = ref(null);
 
-const boxRef: ShallowRef<TresObject | null> = shallowRef(null!);
+const boxRef: ShallowRef<TresObject | null> = shallowRef(null);
 
-
-const { scene } = await useLoader(
-  GLTFLoader,
-  '/sheep_1.glb'
-); //access .glb file
+const { scene } = await useLoader(GLTFLoader, "/sheep_1.glb"); //access .glb file
 
 scene.position.y = -20;
-console.log(scene.children[4].children[1], lightRef)
+console.log(scene.children[4].children[1], lightRef);
 
 const { onLoop } = useRenderLoop();
 
@@ -46,27 +42,26 @@ onLoop(({ delta, elapsed }) => {
       power-preference="high-performance"
       preserve-drawing-buffer
     >
-    <!-- <OrbitControls /> -->
-     
+      <!-- <OrbitControls /> -->
+
       <TresPerspectiveCamera
         :position="[1, 4, 7]"
         :fov="75"
         :near="0.1"
         :far="1000"
       />
-      <TresScene >
-        
-        <!-- <TresMesh ref="boxRef" v-bind="scene"> -->
-        <!-- <TresBoxGeometry :args="[1, 1, 1]" />
-        <TresMeshNormalMaterial /> -->
-      <!-- </TresMesh> -->
+      <TresScene>
+        <TresMesh ref="boxRef" v-bind="scene">
+          <TresBoxGeometry :args="[1, 1, 1]" />
+          <TresMeshNormalMaterial />
+        </TresMesh>
         <TresDirectionalLight
-          
+        ref="lightRef"
           :position="[0, 0, 4]"
-          :intensity="1.5"
+          :intensity="1.7"
           :look-at="[0, 0, 0]"
         />
-        
+
         <TresAxesHelper />
       </TresScene>
     </TresCanvas>
